5241ad665cf5eab34543cce8b4a1ab5aaabe92a1
Tyler Vermillion <mtvextreme1998@yahoo.com>
2017-04-07 15:56:10 -0400

    Added Details

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -98,5 +98,21 @@ namespace RestaurantRater.Controllers
             }
             return View(restaurant);
         }
+
+        //GET Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
     }
 }
